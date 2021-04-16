f7537a3d364f51bb5bef1b42db9f65b68424158e
Anatoliy <tolxpams@gmail.com>
2021-04-16 23:23:28 +0300

    создал VC и пошел спать

--- HW2.6/Control/PersonList1VC.swift
+++ HW2.6/Control/PersonList1VC.swift
@@ -16,17 +16,29 @@ class PersonList1VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         table.dataSource = self
+        table.delegate = self
+        
     }
     
 }
 
-extension PersonList1VC:UITableViewDataSource {
+extension PersonList1VC:UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.datas.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
-        cell.textLabel?.text = "\(data.datas[indexPath[1]].name) \(data.datas[indexPath[1]].surname)"
-        return cell
+        var cell = table.dequeueReusableCell(withIdentifier: idCell)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
+        }
+        cell!.textLabel?.text = "\(data.datas[indexPath.row].name) \(data.datas[indexPath.row].surname)"
+        return cell!
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
